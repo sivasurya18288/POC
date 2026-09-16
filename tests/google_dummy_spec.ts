c748d2bc64d3c8c11bb2c016c9bc33bb2438a07b
SURYA S SIVA <sivasurya18288@gmail.com>
2026-09-16 20:05:36 +0530

    Add dummy Playwright test cases

---
+++ tests/google_dummy_spec.ts
@@ -0,0 +1,12 @@
+import { test, expect } from '@playwright/test';
+
+test('Google Search Page Opens', async ({ page }) => {
+  await page.goto('https://www.google.com');
+
+  await expect(page).toHaveTitle(/Google/);
+
+  await page.locator('textarea[name="q"]').click();
+
+  console.log('Google page opened successfully');
+});
+``
